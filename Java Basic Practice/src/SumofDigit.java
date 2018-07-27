dd746d70f6fd2fb3b7374fe13af61fdabd86b481
KRISHNAN <KRISHNAN@GOPI>
2018-07-27 14:12:43 +0530

    new var

--- Java Basic Practice/src/SumofDigit.java
+++ Java Basic Practice/src/SumofDigit.java
@@ -3,6 +3,7 @@ public class SumofDigit {
 	static void findSum(int input)
 	{
 		int result=0;
+		int var=17;
 		String temp=String.valueOf(input);
 		char[] ary=temp.toCharArray();
 		int even=0,odd=0;
